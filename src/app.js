b2aef784299ad6a4dd8d0c2f2ff7489ac85c095d
Malik Mahmud <almalikmahmud@gmail.com>
2021-04-20 22:03:24 +0100

    attach prisma instance to graphql context

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
+import { PrismaClient } from '@prisma/client';
 import resolvers from './resolvers';
 import typeDefs from './typeDefs';
 
 const app = express();
+const db = new PrismaClient();
 
 app.use('/graphiql', graphqlHTTP((req) => ({
   schema: makeExecutableSchema({ resolvers, typeDefs }),
   context: {
+    db,
     req,
   },
   graphiql: true,
